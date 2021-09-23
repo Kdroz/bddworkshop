0577b98d291fb8c0fe2efefe4ca9db5a163bf0b7
KADOZDEM <kdir.ozdemir@gmail.com>
2021-09-23 13:44:26 +0200

    trying rebase

--- bddworkshop/src/main/java/Main.java
+++ bddworkshop/src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello World !!!");
         System.out.println("Hi pull me");
         System.out.println("My second push!");
+        System.out.println("I will try rebase");
         System.out.println("Try the rebase");
     }
 }
